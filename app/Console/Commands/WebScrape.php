52880d2f174b872219c6dc65c2687e4ab566a662
gofish <gofish@iastate.edu>
2017-09-05 18:17:58 -0500

    Phpstorm helper

--- app/Console/Commands/WebScrape.php
+++ app/Console/Commands/WebScrape.php
@@ -6,34 +6,34 @@ use Illuminate\Console\Command;
 
 class WebScrape extends Command
 {
-  /**
-   * The name and signature of the console command.
-   *
-   * @var string
-   */
-  protected $signature = 'scrape:netnutrition';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scrape:netnutrition';
 
-  /**
-   * The console command description.
-   *
-   * @var string
-   */
-  protected $description = 'Scrape the net nutrition web site';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Scrape the net nutrition web site';
 
-  /**
-   * Create a new command instance.
-   * public function __construct()
-   * {
-   * parent::__construct();
-   * }
-   *
-   * /**
-   * Execute the console command.
-   *
-   * @return mixed
-   */
-  public function handle()
-  {
-    return 1;
-  }
+    /**
+     * Create a new command instance.
+     * public function __construct()
+     * {
+     * parent::__construct();
+     * }
+     *
+     * /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        return 1;
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,24 +7,24 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-  /**
-   * Bootstrap any application services.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    // Hope to remove this in future versions once I have control over MySQL versions
-    Schema::defaultStringLength( 191 );
-  }
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Hope to remove this in future versions once I have control over MySQL versions
+        Schema::defaultStringLength(191);
+    }
 
-  /**
-   * Register any application services.
-   *
-   * @return void
-   */
-  public function register()
-  {
-    //
-  }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
