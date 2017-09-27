cf3242261b98c9d2f60489ff7573f9ef268ba934
gofish <gofish@iastate.edu>
2017-09-27 10:29:51 -0500

    Added in route names

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,16 @@ Route::get('/dashboard', 'DashboardController@dashboard')->name('dashboard');
 Auth::routes();
 
 Route::group(['prefix' => 'api'], function () {
-    Route::get('/', 'ApiController@index');
+    Route::get('/', 'ApiController@index')->name('api.index');
 
     Route::group(['prefix' => 'dining-center'], function () {
-        Route::get('/', 'DiningCenterController@index');
-        Route::get('/{id}', 'DiningCenterController@show');
-        Route::get('/{id}/menus', 'DiningCenterController@showMenus');
-        Route::get('/{id}/foods', 'DiningCenterController@showFoods');
+        Route::get('/', 'DiningCenterController@index')->name('api.dining-center.index');
+        Route::get('/{id}', 'DiningCenterController@show')->name('api.dining-center.show');
+        Route::get('/{id}/menus', 'DiningCenterController@showMenus')->name('api.dining-center.showMenus');
+        Route::get('/{id}/foods', 'DiningCenterController@showFoods')->name('api.dining-center.showFoods');
     });
 
     Route::group(['prefix' => 'menu'], function () {
-        Route::get('/{id}/foods', 'MenuController@showFoods');
+        Route::get('/{id}/foods', 'MenuController@showFoods')->name('api.menu.showFoods');
     });
 });
