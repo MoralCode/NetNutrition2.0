96c8be94955d5a919a49bf33b6599d48cda21b23
gofish <gofish@iastate.edu>
2017-10-01 14:57:04 -0500

    More work with fixing scrape addition

--- api.netnutrition/app/User.php
+++ api.netnutrition/app/User.php
@@ -2,16 +2,13 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 use function bcrypt;
 use function explode;
 use function strpos;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
