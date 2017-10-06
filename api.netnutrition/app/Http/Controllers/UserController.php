68ab5770390597df5f85c49a3f76f33f029392c4
Kyle Nordstrom <Kyle@kyles-mbp.student.iastate.edu>
2017-10-06 16:53:42 -0500

    Implemented functionality of user controller

--- api.netnutrition/app/Http/Controllers/UserController.php
+++ api.netnutrition/app/Http/Controllers/UserController.php
@@ -1,34 +1,21 @@
 <?php
-/*
-|-----------------------------------------------------------------------------------|
-|                                                                                   |
-|  Copyright:                                                                       |
-|     Copyright 2015-2016; Nicholas Brad Dykhuizen. All rights reserved.            |
-|                                                                                   |
-|       NOTICE:  All information contained herein is, and remains the property of   |
-|       Nicholas Brad Dykhuizen and suppliers, if any.                              |
-|       The intellectual and technical concepts contained herein are                |
-|       proprietary to Nicholas Brad Dykhuizen and suppliers and may be             |
-|       covered by U.S. and Foreign Patents, patents in process, and are            |
-|       protected by trade secret or copyright law.                                 |
-|       Dissemination of this information or reproduction of this material          |
-|       is strictly forbidden unless prior written permission is obtained           |
-|       from Nicholas Brad Dykhuizen.                                               |
-|                                                                                   |
-|-----------------------------------------------------------------------------------|
-*/
-
-/**
- * Created by PhpStorm.
- * User: Kyle
- * Date: 10/6/17
- * Time: 3:25 PM
- */
 
 namespace App\Http\Controllers;
-
+use App\User;
 
 class UserController
 {
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function index()
+    {
+        //Eventually want to NOT return passwords and token
+        return User::all();
+    }
 
+    public function show($id)
+    {
+        return User::findorfail($id);
+    }
 }
