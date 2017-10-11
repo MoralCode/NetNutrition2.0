34cec6fea9fb089cefc6052d2fd82886228e9d59
Frank San Filippo <sanflip77@gmail.com>
2017-10-11 15:55:36 -0500

    commit before merge

---
+++ site.netnutrition/src/components/DatePicker.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        
+        <div @click="setDate">
+            <datepicker ref="dp" v-model="dateValue" placeholder=""></datepicker>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return{
+              dateValue: new Date().toLocaleDateString()
+          }
+        },
+        watch:{
+            getDate: function(){
+                this.dateValue = this.$store.state.date.toLocaleDateString();
+            }
+        },
+        methods:{
+            setDate: function(){
+                this.$store.commit('setDate',new Date(this.$refs.dp.parse()));
+            }
+        },
+        mounted() {
+
+            this.$store.commit('setDate', new Date());
+
+        },
+    }
+</script>
