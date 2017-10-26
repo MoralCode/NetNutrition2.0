18919313f206d64a1b549446811b4e56ab3d62c8
Kyle Nordstrom <Kyle@Kyles-MacBook-Pro-8.local>
2017-10-25 21:39:30 -0500

    More eloquent but currently broken adding food logs

--- api.netnutrition/app/Http/Controllers/FoodLogController.php
+++ api.netnutrition/app/Http/Controllers/FoodLogController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Support\Facades\DB;
 use function compact;
 use Carbon\Carbon;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 use function max;
 
@@ -46,46 +47,50 @@ class FoodLogController extends Controller
             ->get();
     }
 
+    /**
+     * @param Request $request
+     * @return array
+     */
     public function addFoodLog(Request $request)
     {
         //Validate the info
-        //might need to check same size array
         $this->validate($request,[
-            //UNCOMMENT LATE
             'foods'=> 'required|array',
             'menus'=> 'required|array'
         ]);
-        //LOGS is equal to the new value needed to enter.
-        $user = $request->user();
-        //$food = $user->food();
+
+        // Check that the sizes of the arrays are equal
+        if(sizeof($request->input('foods')) != sizeof($request->input('menus'))) {
+            return [
+                'foods' => 'Arrays must be of same length',
+                'menus' => 'Arrays must be of same length',
+                'success' => false,
+            ];
+        }
+
+        // Obtain the next db value for a given user's meal block value
         $mealBundle = DB::table('food_logs')
             ->select('*')
-            ->where('user_id', '=', $user->id)
+            ->where('user_id', '=', $request->user()->id)
             ->max('meal_id') + 1;
-        //dd($mealBundle);
 
-        for($i=0; $i<count($request->foods); $i++){
-            DB::table('food_logs')
-                ->insert([
-                    'food_id' => $request->foods[$i],
-                    'menu_id' => $request->menus[$i],
-                    'user_id' => $user->id,
-                    'meal_id' => $mealBundle,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now()
-                ]);
-            };
+        // Loop through the two arrays
+        $menus = $request->input('menus');
+        foreach($request->input('foods') as $index => $food) {
+            $menu = $menus[$index];
+
+            $request->user()->foods()->attatch($food, [
+                'menu_id' => $menu,
+                'meal_id' => $mealBundle,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+
+        // Return the newly added food logs
         return [
             'success' => true,
+            'addedData' => $this->showFoodLog($mealBundle, $request)
         ];
-//            'meal_bundle' => $user
-//            ->foods()
-//            ->attach($user->food
-//            ->id [
-//                'meal_id'=> $mealBundle,
-//
-//
-//                ])
-//        ];
     }
 }
