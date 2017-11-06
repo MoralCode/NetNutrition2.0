b5edcb91bc35e53e0a32ab631be3db943b9163b1
Seth Pipho <seth.pipho@gmail.com>
2017-11-06 16:57:32 -0600

    login thingy

--- site.netnutrition/src/store/index.js
+++ site.netnutrition/src/store/index.js
@@ -7,7 +7,7 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
     state:{
         APIToken:'',
-        loggedIn:false,
+        loggedIn:true,
         foodLog:{},
         diningCenterData:{
             loading:true,
