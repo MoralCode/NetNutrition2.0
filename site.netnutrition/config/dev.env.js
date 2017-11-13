21981715792cbcc3925a48e45e149b53e5dc3f65
Frank San Filippo <sanflip77@gmail.com>
2017-11-13 15:44:51 -0600

    changed dev.env and prod.env api_domain back to actual api_domain

--- site.netnutrition/config/dev.env.js
+++ site.netnutrition/config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_DOMAIN: '"http://api.netnutrition.fchrome"',
+  API_DOMAIN: '"http://demo.netnutrition.trapstats.com"',
 })
