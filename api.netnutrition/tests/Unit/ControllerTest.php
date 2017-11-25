b7c1140d2f648261ba213bfd8c8e55cdb2222e5c
gofish <gofish@iastate.edu>
2017-11-25 14:00:43 -0600

    Implemented controller test

--- api.netnutrition/tests/Unit/ControllerTest.php
+++ api.netnutrition/tests/Unit/ControllerTest.php
@@ -3,11 +3,19 @@
 namespace Unit;
 
 use TestCase;
+use function var_dump;
 
 class ControllerTest extends TestCase
 {
     public function testIndex()
     {
         $this->get('/')->assertResponseStatus(404);
+    }
+
+    public function testUnauthorized()
+    {
+        $this->get('/check-authorized');
+
+        $this->assertFalse(json_decode($this->response->getContent(), true)['authorized']);
     }
 }
