6377cd60b2165e0b0a292d6517a0277810b9c801
gofish <gofish@iastate.edu>
2017-11-25 14:40:08 -0600

    Finished food controller test

--- api.netnutrition/tests/Unit/FoodControllerTest.php
+++ api.netnutrition/tests/Unit/FoodControllerTest.php
@@ -2,12 +2,42 @@
 
 namespace Unit;
 
+use App\Food;
+use App\Nutrition;
 use TestCase;
 
 class FoodControllerTest extends TestCase
 {
     public function testIndex()
     {
-        $this->get('/')->assertResponseStatus(404);
+        $this->get('/food')
+            ->assertResponseOk();
+    }
+
+    public function testShow()
+    {
+        $food = Food::all()->first();
+
+        $this->get("/food/{$food->id}")
+            ->assertResponseOk();
+
+        $this->get('/food/0')
+            ->assertResponseStatus(404);
+    }
+
+    public function testShowNutritions()
+    {
+        $food = Food::all()->first();
+
+        $this->get("/food/{$food->id}/nutritions")
+            ->assertResponseOk();
+
+        foreach (json_decode($this->response->getContent(), true) as $nutrition) {
+            $nutrition = Nutrition::find($nutrition['id']);
+            /** @var Nutrition $nutrition */
+            $this->assertContains($food->id, $nutrition->food()->get()->map(function ($food) {
+                return $food->id;
+            }));
+        }
     }
 }
