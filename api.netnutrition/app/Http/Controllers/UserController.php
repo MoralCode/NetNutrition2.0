d44fda1b69d28849a014f9692a484e4befe63482
Kyle Nordstrom <Kyle@kyles-mbp.student.iastate.edu>
2017-12-02 17:48:14 -0600

    Update User now allows nulls and the same netid as prior

--- api.netnutrition/app/Http/Controllers/UserController.php
+++ api.netnutrition/app/Http/Controllers/UserController.php
@@ -42,6 +42,39 @@ class UserController extends ApiController
      */
     public function update($id, Request $request)
     {
+//        $this->validate($request, [
+//            'net_id' => 'string|unique:users,net_id',
+//            'role_id' => "integer|between:" . Role::ADMIN . "," . Role::STUDENT,
+//        ]);
+
+        //dd('here');
+        $NetID = $request->input('net_id');
+//        //If user inputs the same username they already have
+//        if ($NetID == User::findorfail($id)->net_id){
+//            $NetID = null;
+//        };
+        if ($NetID == null){
+            //No net id specified so set to what it was
+            $NetID = User::findorfail($id)->net_id;
+            //Only validate the role id
+            $this->validate($request, [
+            'role_id' => "integer|between:" . Role::ADMIN . "," . Role::STUDENT,
+            ]);
+        };
+
+        $RoleID = $request->input('role_id');
+        if ($RoleID == null){
+            //No role id specified so set to what it was
+            $RoleID = User::findorfail($id)->role_id;
+            //Only validate the net_id
+            $this->validate($request, [
+            'net_id' => 'string|unique:users,net_id',
+            ]);
+        };
+
+        //
+        if($NetID != null && $RoleID != null && $NetID != User::findorfail($id)->net_id)
+            //dd($NetID);
         $this->validate($request, [
             'net_id' => 'string|unique:users,net_id',
             'role_id' => 'integer|between:1,3',
@@ -50,8 +83,8 @@ class UserController extends ApiController
         return [
             'success' => User::findOrFail($id)
                 ->update([
-                    'net_id' => $request->input('net_id'),
-                    'role_id' => $request->input('role_id'),
+                    'net_id' => $NetID,
+                    'role_id' => $RoleID,
                 ]),
         ];
     }
