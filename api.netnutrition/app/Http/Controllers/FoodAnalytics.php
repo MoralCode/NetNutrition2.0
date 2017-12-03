77e62409c9734aad8e19a89120bfec747e9d7a42
Kyle Nordstrom <Kyle@kyles-mbp.student.iastate.edu>
2017-12-03 16:25:22 -0600

    Most Foods now returns by total number of servings instead of entries

--- api.netnutrition/app/Http/Controllers/FoodAnalytics.php
+++ api.netnutrition/app/Http/Controllers/FoodAnalytics.php
@@ -14,11 +14,11 @@ class FoodAnalytics extends ApiController
         return DB::select('
             SELECT
               *,
-              COUNT(fl.food_id) AS numberEntries
+              SUM(fl.servings) AS totalServings
             FROM food_logs AS fl
               LEFT JOIN foods AS f ON f.id = fl.food_id
             GROUP BY fl.food_id
-            ORDER BY numberEntries DESC
+            ORDER BY totalServings DESC
             LIMIT 10;
         ');
     }
