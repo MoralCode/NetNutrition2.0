00d193bb7ca26a36bc20e3b839028f1358aff2cc
Kyle Nordstrom <Kyle@Kyles-MacBook-Pro-8.local>
2017-12-03 13:37:04 -0600

    Cleaned up user editing

--- api.netnutrition/app/Http/Controllers/UserController.php
+++ api.netnutrition/app/Http/Controllers/UserController.php
@@ -55,51 +55,6 @@ class UserController extends ApiController
      */
     public function update($id, Request $request)
     {
-//        $this->validate($request, [
-//            'net_id' => 'sometimes|string|unique:users,net_id',
-//            'role_id' => "sometimes|integer|between:" . Role::ADMIN . "," . Role::STUDENT,
-//        ]);
-
-//        //dd('here');
-//        $NetID = $request->input('net_id');
-////        //If user inputs the same username they already have
-////        if ($NetID == User::findorfail($id)->net_id){
-////            $NetID = null;
-////        };
-//        if ($NetID == null){
-//            //No net id specified so set to what it was
-//            $NetID = User::findorfail($id)->net_id;
-//            //Only validate the role id
-//            $this->validate($request, [
-//            'role_id' => "integer|between:" . Role::ADMIN . "," . Role::STUDENT,
-//            ]);
-//        };
-//
-//        $RoleID = $request->input('role_id');
-//        if ($RoleID == null){
-//            //No role id specified so set to what it was
-//            $RoleID = User::findorfail($id)->role_id;
-//            //Only validate the net_id
-//            $this->validate($request, [
-//            'net_id' => 'string|unique:users,net_id',
-//            ]);
-//        };
-//
-//        //
-//        if($NetID != null && $RoleID != null && $NetID != User::findorfail($id)->net_id)
-//            //dd($NetID);
-//        $this->validate($request, [
-//            'net_id' => 'string|unique:users,net_id',
-//            'role_id' => "integer|between:" . Role::ADMIN . "," . Role::STUDENT,
-//        ]);
-//
-//        return [
-//            'success' => User::findOrFail($id)
-//                ->update([
-//                    'net_id' => $NetID,
-//                    'role_id' => $RoleID,
-//                ]),
-//        ];
         $user = User::findOrFail($id);
 
         $this->validate($request, [
@@ -113,7 +68,6 @@ class UserController extends ApiController
                 'role_id' => $request->input('role_id', $user->role_id),
             ]),
         ];
-
     }
 
     /**
